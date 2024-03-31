53722cacc83f54a6ee9edd6c145c3b60ddbe5e04
YriiDanilov <Relantii@mail.ru>
2024-03-31 13:58:11 +0300

    add check

--- __tests__/calculateDistance.test.js
+++ __tests__/calculateDistance.test.js
@@ -1,16 +1,26 @@
-import calculateDistance from '../src/calculateDistance.js';
+import { calculateDistance } from '../src/calculateDistance.js';
 
 describe('distance', () => {
 
     let point1;
     let point2;
 
-    beforeEach(()=> {
+    test('To find distance between two points', () => {
         point1 = [0, 0];
         point2 = [3, 4]
+        expect(calculateDistance(point1, point2)).toBe(5)
+    })
+
+    test('Point is not full', () => {
+        point1 = [0, ];
+        point2 = [3, 4]
+        expect(() => calculateDistance(point1, point2)).toThrow('Type correct point')
     })
 
     test('To find distance between two points', () => {
-        expect(calculateDistance(point1, point2)).toBe(5)
+        point1 = [-1, -4]; //
+        point2 = [-1, -10] //
+        expect(calculateDistance(point1, point2)).toBe(6)
     })
+
 })
