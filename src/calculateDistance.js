9383c800ff63caf74a8cb80feaeee52ec7dfdb5b
YriiDanilov <Relantii@mail.ru>
2024-03-31 13:58:44 +0300

    add guard expression

--- src/calculateDistance.js
+++ src/calculateDistance.js
@@ -2,6 +2,9 @@ export const point1 = [0, 0];
 export const point2 = [3, 4];
 
 export const calculateDistance = (point1, point2) => {
+    if (point1.length < 2 || point1.length < 2) {
+        throw new Error('Type correct point');
+    }
     let x = Math.pow((point2[0] - point1[0]), 2)
     let y = Math.pow((point2[1] - point1[1]), 2)
     return Math.sqrt(x + y)
